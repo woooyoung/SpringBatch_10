a9f94133d4824af2523a2b54b93d25f73abd536e
woooyoung <axdsw121@gmail.com>
2024-10-16 14:12:44 +0900

    지난 Job에서 실패한 Step만 재시도, 파라미터가 같도록

--- src/main/java/com/koreait/exam/springbatch_10/job/HelloWorld/HelloWorldJobConfig.java
+++ src/main/java/com/koreait/exam/springbatch_10/job/HelloWorld/HelloWorldJobConfig.java
@@ -22,7 +22,7 @@ public class HelloWorldJobConfig {
     @Bean
     public Job helloWorldJob() {
         return jobBuilderFactory.get("helloWorldJob")
-                .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID를 실행할 때 파라미터로 부여
+//                .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID를 실행할 때 파라미터로 부여
                 .start(helloWorldStep1())
                 .next(helloWorldStep2())
                 .build();
@@ -61,7 +61,7 @@ public class HelloWorldJobConfig {
         return (stepContribution, chunkContext) -> {
             System.out.println("헬로월드 222222222222!!!");
 
-            if(true){
+            if(false){
                 throw new Exception("실패 : 헬로월드 태스클릿 2 실패");
             }
 
