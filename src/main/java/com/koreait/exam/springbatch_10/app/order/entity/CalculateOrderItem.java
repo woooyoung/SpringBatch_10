1ad5d5b839b2402a37d2067e01645db08d58522f
woooyoung <axdsw121@gmail.com>
2024-10-21 15:44:55 +0900

    주문 날짜를 정산 테이블에 추가, 데이터 묶음

--- src/main/java/com/koreait/exam/springbatch_10/app/order/entity/CalculateOrderItem.java
+++ src/main/java/com/koreait/exam/springbatch_10/app/order/entity/CalculateOrderItem.java
@@ -10,6 +10,8 @@ import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
 
+import java.time.LocalDateTime;
+
 import static javax.persistence.FetchType.LAZY;
 
 @Entity
@@ -50,10 +52,21 @@ public class CalculateOrderItem extends BaseEntity {
     private String productName;
 
     // 상품 옵션
-    private String productOptionColor;
-    private String productOptionSize;
-    private String productOptionDisplayColor;
-    private String productOptionDisplaySize;
+//    private String productOptionColor;
+//    private String productOptionSize;
+//    private String productOptionDisplayColor;
+//    private String productOptionDisplaySize;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "color", column = @Column(name = "product_option_color")),
+            @AttributeOverride(name = "size", column = @Column(name = "product_option_size")),
+            @AttributeOverride(name = "displayColor", column = @Column(name = "product_option_dispaly_color")),
+            @AttributeOverride(name = "displaySize", column = @Column(name = "product_option_dispaly_size")),
+    })
+    private CalculateOrderItem.EmbProductOption embProductOption;
+
+    private LocalDateTime orderItemCreatedDate;
 
     public CalculateOrderItem(OrderItem orderItem) {
         this.orderItem = orderItem;
@@ -72,11 +85,31 @@ public class CalculateOrderItem extends BaseEntity {
         //상품
         productName = orderItem.getProductOption().getProduct().getName();
         //상품 옵션
-        productOptionColor = orderItem.getProductOption().getColor();
-        productOptionSize = orderItem.getProductOption().getSize();
-        productOptionDisplayColor = orderItem.getProductOption().getDisplayColor();
-        productOptionDisplaySize = orderItem.getProductOption().getDisplaySize();
+//        productOptionColor = orderItem.getProductOption().getColor();
+//        productOptionSize = orderItem.getProductOption().getSize();
+//        productOptionDisplayColor = orderItem.getProductOption().getDisplayColor();
+//        productOptionDisplaySize = orderItem.getProductOption().getDisplaySize();
 
+        embProductOption = new EmbProductOption(orderItem.getProductOption());
 
+        // 주문 품목이 생성된 시각
+        orderItemCreatedDate = orderItem.getCreateDate();
     }
+
+    @Embeddable
+    @NoArgsConstructor
+    public static class EmbProductOption {
+        private String color;
+        private String size;
+        private String displayColor;
+        private String displaySize;
+
+        public EmbProductOption(ProductOption productOption) {
+            color = productOption.getColor();
+            size = productOption.getSize();
+            displayColor = productOption.getDisplayColor();
+            displaySize = productOption.getDisplaySize();
+        }
+    }
+
 }
